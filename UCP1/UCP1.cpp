262b5cffe9460e289c98e374046efe88a4795a04
LENOVO <MeutyaAzzahrani>
2024-05-05 18:18:33 +0700

    ucp 1

---
+++ UCP1/UCP1.cpp
@@ -0,0 +1,38 @@
+/*
+1. Integer NIM, String Nama, Float nilai
+
+2. for (int i = 0; i < 3; i++)
+
+3. while(bilangan % 2)
+
+4. if(nBilangan % 2 == 0){
+    status = "Genap";
+
+5. struct Mahasiswa {
+    string nim;
+    string nama;
+    DetailAlamat alamat;
+    int umur;
+*/
+
+#include <iostream>
+using namespace std;
+
+struct KualitasUdara() {
+    int nNamaKota;
+    string status;
+}
+int main() {
+    KualitasUdara[2];
+
+    if (int i = 0; i < 75; i++) {
+        status = "Sehat";
+    }
+    else {
+        status = "Tidak sehat";
+    }
+    cout << "Nama Kota " << NamaKota << endl;
+    cout << "statusnya adalah " << status << endl;
+    return 0;
+
+}
